a87b5292cfda6483e57c8a1476b749f503312d8f
Ivan Rodriguez <ivan.rodriguez.o@gmail.com>
2014-05-22 03:51:19 -0500

    Funcionando inicial

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-Mis compras Banorte
+Buynorthe
 ===================
 
-Pequeña clase con app de ejemplo para obtener las notificaciones de compra de Banorte que se envían al mail. Tiene un ejemplo de uso con GMail. Banorte y otros bancos tienen un servicio en el que envían a tu correo electrónico una notificación cada vez que se realiza alguna compra con tu tarjeta, ahí mismo ponen el monto, la fecha y el lugar. Este correo tiene un formato de HTML lo cuál lo hace todavía más sencillo de parsear con algunas extensiones nativas o librerías.
+Pequeña clase con app de ejemplo para obtener las notificaciones de compra de Banorte que se envían al mail.
+Banorte y otros bancos tienen un servicio en el que envían a tu mail una notificación cada vez que se realiza alguna compra con tu tarjeta, ahí mismo ponen el monto, la fecha y el lugar.
+Este mail tiene un formato de HTML lo cuál lo hace todavía más fácil de parsear con algunas extensiones nativas o librerías.
+
+Se necesita IMAP para PHP, en ubuntuni es con:
+
+    sudo apt-get install php5-imap
+
+Crear un archivo ```auth.config```, esto porque está en el .gitgnore para los más weyes como Johny les evita problemas de subir contraseñas y usuarios al Git.
+También se podría poner en otra carpeta que esté fuera de la del server. El archivo es así:
+
+```
+    <?php
+        define('nutzer', '<Vuestro usuario de GMail>@gmail.com');
+        define('kenn', '<Password súper secreto>');
+    ?>
+```
+
+### TODO
+
+ - Usar OAuth si hay para GMail, IMAP nein
+ - Exportar a CSV / Excel / Pdf
+ - Estadísticas de lugares, fechas, montos
--- index.php
+++ index.php
@@ -1,27 +1,49 @@
 <?php
-# Hay muchas copias por ahí pero parece ser que la idea original viene de
+
 # http://davidwalsh.name/gmail-php-imap
 
+require_once './auth.config';
+require_once __DIR__ . '/vendor/autoload.php';
+
 $hostname = '{imap.gmail.com:993/imap/ssl}INBOX';
-$username = 'gibtsnicht@gmail.com';
-$password = 'DasIstNetMeinKennwortDuScherzkeks';
+$username = NUTZER;
+$password = KENN;
 
 try {
-    $inbox = imap_open($hostname,$username,$password) or die('Error al conectarse a GMail: ' . imap_last_error());
-} catch(Exception $e) {
-    echo 'Debes tener la lib IMAP para PHP instalada';
+    $inbox = imap_open($hostname, $username, $password) or die('Error imap GMail: ' . imap_last_error());
+} catch (Exception $e) {
+    echo 'Debes tener la lib IMAP para PHP instalada: <p></p><pre>' . $e . '</pre>';
 }
 
 $emails = imap_search($inbox, 'ALL SUBJECT "Notificacion de Compra en Comercio"');
 
-if($emails) {	
-    foreach($emails as $email_number) {
-	    $overview = imap_fetch_overview($inbox,$email_number,0);
-	    $message = imap_fetchbody($inbox,$email_number,1);
-	    echo '<pre>';
-        var_dump($message);
-		echo '</pre>';
-	}
-} 
-imap_close($inbox);
+$mailexanders = array();
+if ($emails) {
+    foreach ($emails as $email_number) {
+        $message = imap_fetchbody($inbox, $email_number, 1);
+        $mailexanders[] = $message;
+    }
+    imap_close($inbox);
+    # Para todos los mensajes
+    $hunter = new \Ivansabik\DomHunter\DomHunter();
+
+    foreach ($mailexanders as $delarge) {
+        $hunter->strHtmlObjetivo = $delarge;
+        $presas = array();
+        $presas[] = array('id_compra', new Ivansabik\DomHunter\IdUnico(6, 'num'));
+        $presas[] = array('operacion', new Ivansabik\DomHunter\KeyValue('Operacion:', TRUE, TRUE));
+        $presas[] = array('fecha_hora', new Ivansabik\DomHunter\KeyValue('Fecha y hora de la operacion'));
+        $presas[] = array('fecha_aplicacion', new Ivansabik\DomHunter\KeyValue('Fecha de Aplicacion'));
+        $presas[] = array('monto', new Ivansabik\DomHunter\KeyValue('Importe'));
+        $presas[] = array('canal_operacion', new Ivansabik\DomHunter\KeyValue('Canal de operacion'));
+
+        $hunter->arrPresas = $presas;
+        echo '<pre>';
+        print_r($hunter->hunt());
+        echo '</pre>';
+    }
+    # Cada mensaje
+} else {
+    'No hay mails';
+}
 ?>
